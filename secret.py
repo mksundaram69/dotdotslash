c0af32a7f2a435c40225d3c0b75b8263e6eddc07
MK SUNDARAM <55274010+mksundaram69@users.noreply.github.com>
2023-09-21 19:07:42 +0800

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,7 @@
+# Guest only login
+# Used for Login as Guest button and login the current session as GUEST
+APP_GUEST_USERNAME = "MK.sundaram"
+APP_GUEST_PASSWORD = "mks123456"
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '10-6d$q$&xxc9heb(hjeda$w%7pxC#+hhx%b0xl&_q(p8(u#uu'
